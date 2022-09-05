0c9a07391d17d7cb591dca8c1791cb2bf8b4dc06
www3087123 <1010098225@qq.com>
2022-09-05 13:58:00 +0800

    第一次提交

---
+++ src/router/IndexRouter.jsx
@@ -0,0 +1,12 @@
+import {Routes,Route} from "react-router-dom"
+import React from 'react'
+
+export default function IndexRouter() {
+  return (
+    <div>
+        <Routes>
+            <Route></Route>
+        </Routes>
+    </div>
+  )
+}
